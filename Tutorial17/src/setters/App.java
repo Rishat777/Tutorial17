ab550fe5c3b2557d73e80089d8e87b1048d2b697
Rishat777 <rishatID7@gmail.com>
2016-12-01 22:46:19 -0500

    Tutorial 17 is added

---
+++ Tutorial17/src/setters/App.java
@@ -0,0 +1,64 @@
+package setters;
+
+class Olympia {
+/*	private String intro;
+	private int age;
+	private String goal;
+
+	public void setIntro(String bio) {
+		this.intro = bio;
+	}
+
+	public void setAge(int yearsPro) {
+		this.age = yearsPro;
+	}
+
+	public void setGoal(String vision) {
+		this.goal = vision;
+	}
+
+	public String getIntro() {
+		return intro;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getGoal() {
+		return goal;
+	}
+
+	public void setInfo(String name, int age) {
+		setIntro(name);
+		setAge(age);
+		
+	}
+}
+	public class App {
+	public static void main(String[] args) {
+		Olympia bodybuilders = new Olympia();
+
+		bodybuilders.setIntro("I am Kevin Levrone. You all know the age of a legend. That's right baby. It's ");
+		bodybuilders.setAge(52);
+		bodybuilders.setGoal("And this Sandow is for you, baby!");
+
+		System.out.println(bodybuilders.getIntro() + bodybuilders.getAge() + "\n" + bodybuilders.getGoal());
+	}*/
+	
+	public static void main (String [] args) {
+		reverseString("WElcome");
+	
+
+	}
+	
+	public static void reverseString (String str) {
+		if ((str==null)||(str. length()<= 1) )
+
+			System.out.println(str);
+
+			String text = new StringBuilder(str).reverse().substring(1) + str. charAt(0);	
+			System.out.println(text);
+			}
+}
+
